3a61ad69cd91ff062dacace8541c0eb8c315222b
unknown <tobilobadurojaiye@gmail.com>
2023-08-06 14:55:31 -0400

    link and page routing

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
-    <section className='section'>
+    <section className="section">
       <h2>Error</h2>
+      <p>404 Page not Found</p>
+      <Link to="/"> Back to Home Page</Link>
     </section>
   );
 };
